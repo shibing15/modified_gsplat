8b6e1ef4b4878085e4982ca683596736f640ec83
YundaSun <170859412@qq.com>
2025-11-15 19:44:44 +0800

    Fix: update default strategy behavior (from detached HEAD)

--- gsplat/strategy/default.py
+++ gsplat/strategy/default.py
@@ -77,7 +77,7 @@ class DefaultStrategy(Strategy):
     grow_scale3d: float = 0.01
     grow_scale2d: float = 0.05
     prune_scale3d: float = 0.1
-    prune_scale2d: float = 0.15
+    prune_scale2d: float = 0.02 #0.15
     refine_scale2d_stop_iter: int = 0
     refine_start_iter: int = 500
     refine_stop_iter: int = 15_000
@@ -330,6 +330,8 @@ class DefaultStrategy(Strategy):
         step: int,
     ) -> int:
         is_prune = torch.sigmoid(params["opacities"].flatten()) < self.prune_opa
+        # is_too_small = (torch.exp(params["scales"]).max(dim=-1).values < 1e-4)
+        # is_prune = is_prune | is_too_small
         if step > self.reset_every:
             is_too_big = (
                 torch.exp(params["scales"]).max(dim=-1).values
